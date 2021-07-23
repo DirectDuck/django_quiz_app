f2c237d4309c04a65e63819b33f9157314cffa4f
dduck <famgui14@gmail.com>
2021-07-24 02:29:23 +0300

    configured templates and static

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+
 import environ
 
 
@@ -45,7 +47,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -100,6 +102,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = tuple(os.path.join(BASE_DIR, "static"))
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
