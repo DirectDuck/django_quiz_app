e028c4b2471257d11be27998793a87e28a9a511d
dduck <famgui14@gmail.com>
2021-07-25 03:17:16 +0300

    fixed settings file

--- config/settings/base.py
+++ config/settings/base.py
@@ -124,6 +124,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
+
 # Authentication
 
 AUTHENTICATION_BACKENDS = [
@@ -133,6 +134,7 @@ AUTHENTICATION_BACKENDS = [
 
 LOGIN_REDIRECT_URL = "pages:home"
 LOGIN_URL = "account_login"
+
 
 # Site
 
