9d34c47d7968daa78e92525210ea61f4607d734b
dduck <famgui14@gmail.com>
2021-07-28 01:05:28 +0300

    added tryout results models

--- apps/takes/models.py
+++ apps/takes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from apps.quizzes.models import Quiz, QuizItemAnswer
+
+
+class CompletedTryout(models.Model):
+    """Model containing information about tryout completed by user"""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="completed_tryout",
+        on_delete=models.CASCADE,
+    )
+
+    quiz = models.ForeignKey(
+        Quiz,
+        related_name="completed_tryout",
+        on_delete=models.CASCADE,
+    )
+
+    score = models.PositiveIntegerField()
+
+
+class CompletedTryoutAnswer(models.Model):
+    """Model to store user answers for tryout"""
+
+    completed_tryout = models.ForeignKey(
+        CompletedTryout,
+        related_name="answers",
+        on_delete=models.CASCADE,
+    )
+
+    item_answer = models.ForeignKey(
+        QuizItemAnswer,
+        on_delete=models.CASCADE,
+    )
