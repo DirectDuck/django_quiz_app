fd98e5184d447d64607da4173e6d5bf31d91303a
dduck <famgui14@gmail.com>
2021-08-01 16:49:52 +0300

    added user completed quiz models

--- apps/takes/models.py
+++ apps/takes/models.py
@@ -73,3 +73,68 @@ class CompletedTryoutAnswer(models.Model):
             completed_tryout=completed_tryout,
             item_answer=item_answer,
         )
+
+
+class CompletedQuiz(models.Model):
+    """Model containing information about quiz completed by user"""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="completed_quizzes",
+        on_delete=models.CASCADE,
+    )
+
+    quiz = models.ForeignKey(
+        Quiz,
+        related_name="completed_quizzes",
+        on_delete=models.CASCADE,
+    )
+
+    score = models.PositiveIntegerField()
+
+    def update_score(self):
+        """Update score based on number of correct answers
+        in related CompletedQuizAnswer objects"""
+
+        self.score = self.answers.filter(item_answer__correct=True).count()
+        self.save(update_fields=["score"])
+
+    def get_result_message(self):
+        """Get result text from QuizResult based on current score"""
+
+        quiz_result = self.quiz.results.filter(score=self.score)
+
+        if not quiz_result.exists():
+            raise Exception(
+                "Something went wrong!"
+                "Are you sure you didn't modified quiz while trying it out?"
+            )
+
+        return quiz_result.first().text
+
+
+class CompletedQuizAnswer(models.Model):
+    """Model to store user answers for tryout"""
+
+    completed_quiz = models.ForeignKey(
+        CompletedQuiz,
+        related_name="answers",
+        on_delete=models.CASCADE,
+    )
+
+    item_answer = models.ForeignKey(
+        QuizItemAnswer,
+        on_delete=models.CASCADE,
+    )
+
+    @classmethod
+    def initialize_from_answer_pk(cls, completed_quiz, item_answer_pk):
+        """Get initialized (but not created in db)
+        CompletedQuizAnswer from CompletedTryout and QuizItemAnswer's pk"""
+
+        item_answer = QuizItemAnswer.objects.get(pk=item_answer_pk)
+
+        return cls(
+            completed_quiz=completed_quiz,
+            item_answer=item_answer,
+        )
