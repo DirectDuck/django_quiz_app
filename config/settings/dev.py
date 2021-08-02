bead441518e946210b519a363705586689a3678e
dduck <famgui14@gmail.com>
2021-08-02 22:36:36 +0300

    explore query fix

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -15,3 +15,10 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
