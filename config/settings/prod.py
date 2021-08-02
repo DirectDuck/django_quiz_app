683347ff885ac0696cdeb123f2a5c299636f30aa
dduck <famgui14@gmail.com>
2021-08-02 19:22:41 +0300

    updated prod middleware

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,4 +3,6 @@ from .base import *
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
 
+MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware"] + MIDDLEWARE
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
