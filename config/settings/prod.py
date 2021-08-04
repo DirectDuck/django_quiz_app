3353281b1f0319043fe199b9145665d4196e529c
dduck <famgui14@gmail.com>
2021-08-04 03:28:24 +0300

    added django-otp to production

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -30,6 +30,26 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = env("EMAIL_SMTP_PASSWORD")
 
 
+# Security
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+CSRF_COOKIE_SECURE = True
+
+
+# Django-admin two-factor auth
+
+INSTALLED_APPS += ["django_otp" "django_otp.plugins.otp_totp"]
+
+MIDDLEWARE += "django_otp.middleware.OTPMiddleware"
+
+
 # Sentry
 
 import sentry_sdk
