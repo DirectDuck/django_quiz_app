a6e5ac626ca8b6ef8152609641a26752cf973f53
dduck <famgui14@gmail.com>
2021-08-09 06:00:43 +0300

    removed browsable api from drf

--- config/settings/base.py
+++ config/settings/base.py
@@ -170,4 +170,5 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 9,
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
